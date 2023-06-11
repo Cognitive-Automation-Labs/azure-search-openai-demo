42ba37b2250ace1077ff88be82dc573a9a8a9ffe
Tylor Bunting <tb@cognitiveautomationlabs.com>
2023-06-11 23:56:52 +1000

    calab.ai small changes added

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "What are some of the specific policies I should be aware of?",
+        value: "What are some of the specific policies I should be aware of?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "What happens in a performance review at Cognitive Automation Labs?", value: "What happens in a performance review at Cognitive Automation Labs?" },
+    { text: "What is the promotion pathway for engineers at Cognitive Automation Labs?", value: "What is the promotion pathway for engineers at Cognitive Automation Labs?" }
 ];
 
 interface Props {
