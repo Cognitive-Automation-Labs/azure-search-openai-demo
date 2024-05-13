714a62b1cd32fdc60e7250f2d4460d75184740e9
Tylor Bunting <tb@cognitiveautomationlabs.com>
2024-05-13 11:22:30 +1000

    improvements to GenAI pipeline + google tag added

--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -184,7 +184,7 @@ const Chat = () => {
                     <div className={styles.chatInput}>
                         <QuestionInput
                             clearOnSend
-                            placeholder="Type a new question (e.g. does my plan cover annual eye exams?)"
+                            placeholder="Type a new question (e.g. what are performance reviews like?)"
                             disabled={isLoading}
                             onSend={question => makeApiRequest(question)}
                         />
